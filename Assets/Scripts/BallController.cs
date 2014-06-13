640b1cacbd3e721663d8373b8a5270742fc911c3
Noah Bannister <noahabannister@gmail.com>
2014-06-13 07:33:44 -0400

    Initial Commit
    Basic setup for ball control and android debug mode

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+
+public class BallController : MonoBehaviour {
+	public float MaxVelocity = 10;
+	public float Torque = 1;
+
+	private Vector3 inputDirection;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.maxAngularVelocity = MaxVelocity;
+	}
+
+	void FixedUpdate () {
+		Vector3 torqueAxis = Vector3.zero;
+		if (Application.platform == RuntimePlatform.Android ) {
+			inputDirection = new Vector3 (Input.acceleration.y, 0, -Input.acceleration.x);
+			torqueAxis = new Vector3 (inputDirection.z, 0, -inputDirection.x) * Torque;
+		}
+		else if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer) {
+			inputDirection = Vector3.ClampMagnitude(new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")),1);
+			torqueAxis = new Vector3 (-inputDirection.x, 0, -inputDirection.z) * Torque;
+		}
+		rigidbody.AddTorque(torqueAxis, ForceMode.Force);
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+//	void OnCollisionEnter () {
+//		Handheld.Vibrate ();
+//	}
+}
---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+	public Transform RedRespawn;
+	public Transform BlueRespawn;
+
+	void Awake () {
+		// Disable screen dimming
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.GetComponent<BallController>() != null) {
+			if (col.gameObject.name == "Red Player")
+				col.gameObject.transform.position = RedRespawn.position;
+			else if (col.gameObject.name == "Blue Player")
+				col.gameObject.transform.position = BlueRespawn.position;
+			col.gameObject.rigidbody.angularVelocity = Vector3.zero;
+			col.gameObject.rigidbody.velocity = Vector3.zero;
+			Handheld.Vibrate ();
+		}
+	}
+}
